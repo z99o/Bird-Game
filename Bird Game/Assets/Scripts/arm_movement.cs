34579e01881e5eec35b45e48d41f841564c1cd71
z99o <z99o@users.noreply.github.com>
2018-12-26 18:58:11 -0800

    Updates to Arm Movement

--- Bird Game/Assets/Scripts/arm_movement.cs
+++ Bird Game/Assets/Scripts/arm_movement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class arm_movement : MonoBehaviour
 {
+    protected Transform T;
+    protected virtual void Awake() { T = transform; } //caches original transform, this might be useful later.
     // Start is called before the first frame update
+    public float stretchSpeed = 0.1f;
+    public float stretchDistance = 15;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();        
+    }
+    void movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Horizontal") && transform.localScale.x < stretchDistance)
+        {
+            float movement = stretchSpeed * moveHorizontal;
+            Vector2 scale = transform.localScale;
+            scale.x += movement;
+            transform.localScale = scale;
+            transform.position = (Vector2)transform.position + Vector2.left * (scale);
+        }   
+
     }
 }
