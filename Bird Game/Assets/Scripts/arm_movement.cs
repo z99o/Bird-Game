30a520897a9b89276ee866485351113f0ab6053e
z99o <z99o@users.noreply.github.com>
2018-12-28 00:38:41 -0800

    Rotation Now works
    ^ yepperoo

--- Bird Game/Assets/Scripts/arm_movement.cs
+++ Bird Game/Assets/Scripts/arm_movement.cs
@@ -8,30 +8,45 @@ public class arm_movement : MonoBehaviour
     protected virtual void Awake() { T = transform; } //caches original transform, this might be useful later.
     // Start is called before the first frame update
     public float stretchSpeed = -0.001f;
-    public float stretchMin = 0.002f;
+    public float stretchMin = 0.2f;
     public float stretchMax = 1;
-    void Start(){
-
+    public float rotateMax = 95;
+    public float rotateMin = -95;
+    public float rotateSpeed = 10;
+    void Start() {
+        transform.Rotate(0, 0, 360);
     }
-
     // Update is called once per frame
     void Update(){
-        movement();
+        armStretch();
+        armRotate();
     }
-    void movement(){
+    void armStretch(){
             //arm can not have an x scale smaller than stretchmin, and cannot be greater than stretchmax
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            //float moveHorizontal = Input.GetAxis("Horizontal");
+            //float moveVertical = Input.GetAxis("Vertical");
             Vector2 scale = transform.localScale;
-            if (Input.GetKey("left") && transform.localScale.x < stretchMax){
+           if (Input.GetKey("left") && transform.localScale.x < stretchMax){
                 scale.x += stretchSpeed * Time.deltaTime;
                 Debug.Log("left held down,");
                 transform.localScale = scale;
            }
            if (Input.GetKey("right") && transform.localScale.x > stretchMin){
-            scale.x += (-stretchSpeed) * Time.deltaTime;
-            Debug.Log("right held down,");
-            transform.localScale = scale;
+                scale.x += (-stretchSpeed) * Time.deltaTime;
+                Debug.Log("right held down,");
+                transform.localScale = scale;
+        }
+    }
+    void armRotate() {
+        Debug.Log("transform.localRotation.z " + transform.localRotation.z);
+        if (Input.GetKey("up") && transform.localRotation.z <= rotateMin) {
+            transform.Rotate(0, 0, -Time.deltaTime * rotateSpeed);
+            Debug.Log("up held down");
+        }
+        if (Input.GetKey("down") && transform.localRotation.z >= rotateMax)
+        {
+            transform.Rotate(0,0,Time.deltaTime*rotateSpeed);
+            Debug.Log("down held down,");
         }
     }
 }
