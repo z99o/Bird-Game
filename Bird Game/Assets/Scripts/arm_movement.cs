a044ca0691f0cca79e1057a796e828053ec7e2d6
z99o <z99o@users.noreply.github.com>
2019-01-02 19:28:18 -0800

    Added Second Arm + More
    Second arm added, arm script now detects which arm it is attached to upon startup, and gives appropriate control.
    
    Body added, does nothing other than provide a base for the arms.
    
    Tags added and sorting layers added to give the correct effects

--- Bird Game/Assets/Scripts/arm_movement.cs
+++ Bird Game/Assets/Scripts/arm_movement.cs
@@ -16,7 +16,20 @@ public class arm_movement : MonoBehaviour
     public float rotateMax = 95;
     public float rotateMin = -95;
     public float rotateSpeed = 10;
+    private string extend, retract, up, down;
     void Start() {
+        if(this.tag == "rightArm"){
+            extend = "a";
+            retract = "d";
+            up = "w";
+            down = "s";
+        }
+        else{
+            extend = "left";
+            retract = "right";
+            up = "up";
+            down = "down";
+        }
         transform.Rotate(0, 0, 360);
     }
     // Update is called once per frame
@@ -29,12 +42,12 @@ public class arm_movement : MonoBehaviour
             //float moveHorizontal = Input.GetAxis("Horizontal");
             //float moveVertical = Input.GetAxis("Vertical");
             Vector2 scale = transform.localScale;
-           if (Input.GetKey("left") && transform.localScale.x < stretchMax){
+           if (Input.GetKey(extend) && transform.localScale.x < stretchMax){
                 scale.x += (stretchSpeed * Time.deltaTime)*Math.Abs((float)Math.Log(stretchSlowdown*transform.localScale.x));
                 Debug.Log("left held down,");
                 transform.localScale = scale;
            }
-           if (Input.GetKey("right") && transform.localScale.x > stretchMin){
+           if (Input.GetKey(retract) && transform.localScale.x > stretchMin){
                 scale.x += (-stretchSpeed * Time.deltaTime)*Math.Abs(((float)Math.Log(transform.localScale.x*stretchSnapback)));
                 Debug.Log("right held down,");
                 transform.localScale = scale;
@@ -42,11 +55,11 @@ public class arm_movement : MonoBehaviour
     }
     void armRotate() {
         Debug.Log("transform.localRotation.z " + transform.localRotation.z);
-        if (Input.GetKey("up") && transform.localRotation.z <= rotateMin) {
+        if (Input.GetKey(up) && transform.localRotation.z <= rotateMin) {
             transform.Rotate(0, 0, -Time.deltaTime * rotateSpeed);
             Debug.Log("up held down");
         }
-        if (Input.GetKey("down") && transform.localRotation.z >= rotateMax)
+        if (Input.GetKey(down) && transform.localRotation.z >= rotateMax)
         {
             transform.Rotate(0,0,Time.deltaTime*rotateSpeed);
             Debug.Log("down held down,");
